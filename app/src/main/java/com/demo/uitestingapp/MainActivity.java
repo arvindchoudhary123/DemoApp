bfe03d3ea2e0c0a5b7872e463d42358e1b9e986a
arvind1505 <yari.achoudhary@gmail.com>
2019-10-17 17:54:26 +0530

    FirstApp

---
+++ app/src/main/java/com/demo/uitestingapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.demo.uitestingapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+   Button btn;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
